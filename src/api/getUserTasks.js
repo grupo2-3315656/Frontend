6fd5f4c39a53c13d0198f20906474fd6a6afa2bc
Jhon-Gelvez <jhonjairogelvezgomez@gmail.com>
2026-07-11 14:45:28 -0500

    feat(api): ajustar las api para que sean compatibles con las nuevas rutas (#2) (#25)

--- src/api/getUserTasks.js
+++ src/api/getUserTasks.js
@@ -2,9 +2,9 @@ import { apiUrl } from "../services/config.js";
 import { fetchApi } from "../utils/fetchApi.js";
 
 export const getUserTasks = async (userId) => {
-    const data = await fetchApi(`${apiUrl}/${userId}?_embed=tasks`, {}, {
+    const data = await fetchApi(`${apiUrl}/${userId}/tasks`, {}, {
         500: "Error interno del servidor al obtener tareas",
     }, "Error al obtener las tareas");
 
-    return data.tasks || [];
+    return data || [];
 };
